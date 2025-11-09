73f6c256ae885e516ef24837e2d468af85cbc866
Jet Pham <jetthomaspham@gmail.com>
2025-11-08 16:14:05 -0800

    feat: add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,30 @@ import { Geist } from "next/font/google";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
-  title: "FBI Wanted",
-  description: "Browse FBI Wanted Persons database",
+  title: "FBI Most Wanted",
+  description: "Browse FBI Wanted Persons Browser",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    title: "FBI Wanted",
+    description: "Browse FBI Wanted Persons Browser",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "FBI Wanted",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FBI Wanted",
+    description: "Browse FBI Wanted Persons Browser",
+    images: ["/opengraph-image.png"],
+  },
+  other: {
+    "apple-mobile-web-app-title": "FBI Wanted",
+  },
 };
 
 const geist = Geist({
